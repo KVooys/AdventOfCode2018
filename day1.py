a6b414e7c4bb8434b271db700d122fb5ccc4e034
Kurt Vooys <k.vooys@hotmail.com>
2018-12-01 14:53:42 +0100

    Made a cleaner version of day 1 that only opens the input file once.

--- day1.py
+++ day1.py
@@ -2,28 +2,25 @@
 """
 Machine is locked by frequency drift.
 A file contains +X and -X commands, which means the frequency needs to change by that much.
-Part 1: Find the final frequency after all changes, solved by splitting the commands on the first char.
+Part 1: Find the final frequency after all changes, solved by simply looping over the commands.
 Part 2: Find the frequency that's reached twice first; solved by making the function recursive and storing a set of
 already reached frequencies.
 """
 
 
-def run_through_list(start, reached):
+def run_through_list(start, reached, changes):
     frequency = start
-    with open("inputs/day1.txt", "r") as file:
-        for line in file:
-            op, change = line[0], int(line[1:])
-            if op == "+":
-                frequency += change
-            else:
-                frequency -= change
-            if frequency in reached:
-                print(frequency)
-                exit()
-            else:
-                reached.add(frequency)
-            print(op, change, frequency)
-    run_through_list(frequency, reached)
+    for change in changes:
+        frequency += int(change)
+        if frequency in reached:
+            print(frequency)
+            exit()
+        else:
+            reached.add(frequency)
+        print(change, frequency)
+    run_through_list(frequency, reached, changes)
 
 
-run_through_list(0, {0})
+with open("inputs/day1.txt", "r") as file:
+    lines = file.readlines()
+run_through_list(0, {0}, lines)
